208c0654df4b0d82e25434b3fce1ca2a4a5e4d4a
Shohjaxon <118268074+Shohjaxon-098@users.noreply.github.com>
2023-12-10 22:05:53 +0500

    s

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:web_source/widgets/key.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +12,16 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(Icons.location_on_rounded),
+            Text(""),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      drawer: Drawer(),
       body: Container(),
     );
   }
---
+++ lib/screens/drawer_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DrawerScreen extends StatefulWidget {
+  const DrawerScreen({super.key});
+
+  @override
+  State<DrawerScreen> createState() => _DrawerScreenState();
+}
+
+class _DrawerScreenState extends State<DrawerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
